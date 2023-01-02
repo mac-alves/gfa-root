1f972fb6c9faec3bf398763ca5289ec878d4e656
Mauricio <maurcioalves@ciandt.com>
2023-01-02 11:36:20 -0300

    modules configuration

--- src/gfa-root-config.ts
+++ src/gfa-root-config.ts
@@ -4,15 +4,17 @@ import {
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
+import microFrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microFrontendLayout);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
 });
+
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
