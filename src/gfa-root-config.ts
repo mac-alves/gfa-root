a641b8645ba3e4d34019cf1092b1db20be9fbee3
Mauricio <maurcioalves@ciandt.com>
2023-01-02 15:22:27 -0300

    cleanup and link with shared module

--- src/gfa-root-config.ts
+++ src/gfa-root-config.ts
@@ -1,13 +1,42 @@
-import { registerApplication, start } from "single-spa";
+import { registerApplication, start, addErrorHandler, getAppStatus, LOAD_ERROR } from "single-spa";
 import {
   constructApplications,
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
 
-import microFrontendLayout from "./microfrontend-layout.html";
+import layout from "./views/layout.html";
+import loader from "./views/loader.html";
 
-const routes = constructRoutes(microFrontendLayout);
+import { publicApiFunction } from '@gfa/gfa-infra';
+
+addErrorHandler(err => {
+  if (getAppStatus(err.appOrParcelName) === LOAD_ERROR) {
+      System.delete(System.resolve(err.appOrParcelName));
+  }
+
+  let el = document.getElementById("single-spa-load-error");
+
+  el.innerHTML = `<div class="spa-err-msg"><div>We're sorry!</div><div>Your request could not be completed at this time. Please try again later.</div></div>`;
+  document.title = 'Single-Spa - Error Loading';
+  console.log('Error loading app. ', err);
+});
+
+window.addEventListener('single-spa:before-app-change', evt => {
+  publicApiFunction();
+  
+  let el = document.getElementById("single-spa-load-error");
+  el.innerHTML = '';
+});
+
+const data = {
+  props: {},
+  loaders: {
+    loader,
+  }
+}
+
+const routes = constructRoutes(layout, data);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
